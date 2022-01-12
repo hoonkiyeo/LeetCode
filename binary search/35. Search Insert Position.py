7d43c30c7df09dc573d2fab54c479bb49355445b
hoonkiyeo <hky1721@gmail.com>
2022-01-11 22:12:47 -0600

    upload 35. search insert position.py

---
+++ binary search/35. Search Insert Position.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+
+        return left
