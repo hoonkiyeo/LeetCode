e89a20e445bcacca26d9d37a0d147303b4d5555b
hoonkiyeo <hky1721@gmail.com>
2022-02-05 20:55:04 -0600

    upload longest common prefix

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,3 +1,17 @@
-strs = ["flower","flow","flight"]
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        size = len(strs)
+        prefix = ""
 
-print(sorted(strs))
+        if not strs:
+            return prefix
+        elif size == 1:
+            return strs[0]
+        else:
+            strs.sort()
+            for x,y in zip(strs[0], strs[-1]):
+                if x == y:
+                    prefix += x
+                else:
+                    break
+        return prefix
