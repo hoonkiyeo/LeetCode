f968a3565fbbbeafd2b3609fff4789919b41a634
hoonkiyeo <hky1721@gmail.com>
2022-02-12 23:40:12 -0600

    modify solution of 142. Linked List Cycle2.py

--- Linked List/142. Linked List Cycle II.py
+++ Linked List/142. Linked List Cycle II.py
@@ -6,15 +6,18 @@
 
 class Solution(object):
     def detectCycle(self, head):
-        if not head:
+        if not head or not head.next:
             return None
-        node = head
-        visited = set()
+        else:
+            node = head
+            visited = set()
 
-        while node is not None:
-            if node in visited:
-                return node
-            else:
+            while True:
+                if node is None or node.next is None:
+                    return None
+                if node in visited:
+                    break
                 visited.add(node)
                 node = node.next
-        return None
+
+        return node
